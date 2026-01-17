402f3375337ce486fa24c6337cc4d7f48c6edbb8
Tejaswi Kottakki <kottakkitejaswi@gmail.com>
2026-01-17 23:48:51 +0530

    Exceptions

---
+++ java-core/01_basics/ExceptionDemo.java
@@ -0,0 +1,14 @@
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index");
+        } finally {
+            System.out.println("Execution completed");
+        }
+    }
+}
